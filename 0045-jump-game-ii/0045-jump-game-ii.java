b6b8fc381b3d514e86024c961ae8da616a206c30
Piyush Garg <pg67824@gmail.com>
2024-12-20 14:46:36 +0530

    Time: 1 ms (99.24%), Space: 44.6 MB (95.00%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n= nums.length;
+        int jumps=0, currentEnd=0, farthest=0;
+        
+        for(int i=0; i<n-1; i++){
+            farthest = Math.max(farthest, i + nums[i]);
+            
+            if(i ==currentEnd){
+                jumps++;
+                currentEnd=farthest;
+                if (currentEnd >= n - 1) break; 
+            }
+        }
+        return jumps;
+    }
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        
+        int[] nums1 = {2, 3, 1, 1, 4};
+        System.out.println(solution.jump(nums1));
+        
+        int[] nums2 = {2, 3, 0, 1, 4};
+        System.out.println(solution.jump(nums2));
+    }
+}
